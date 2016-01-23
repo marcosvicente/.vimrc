a7dfdf67a793c28df352b8e8ce02243bf225eb46
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2016-01-22 23:13:46 -0200

    22/01/2015

--- tcc/core/views.py
+++ tcc/core/views.py
@@ -10,22 +10,23 @@ def index(request):
         form_texto = PostTextoForm(request.POST or None )
         form_imagem = PostImageForm(request.POST or None)
         form_video = PostVideoForm(request.POST or None)
-        if form_evento.is_valid():
-            post = form_evento.save(commit=false)
-            post.usuario = request.user.username
-            post.criado = timezone.now()
-            post.save()
-        else: 
-            form_evento = PostEventosForm()
+
+#        if form_evento.is_valid():
+#            post = form_evento.save(commit=false)
+#           post.usuario = request.user.username
+#            post.criado = timezone.now()
+# post.save()
+ #       else: 
+ #           form_evento = PostEventosForm()
 
        
     return render(request, 'index.html', 
             {
+                'form_imagem':form_imagem,
+                'form_video':form_video,
                 'form_evento':form_evento,
                 'form_texto':form_texto,
-                'form_imagem':form_imagem,
-                'form_video':form_video
-                
+
             }
         )
 
